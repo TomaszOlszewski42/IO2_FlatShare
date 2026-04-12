9cfc6c9d9250292c5ccacdfe75947130e8182246
Tomasz Olszewski <tomaszolszewski42@gmail.com>
2026-04-12 14:22:22 +0200

    Feature/creating listings (#20)
    * Created model for listings, configured db context and create few repositories and services
    
    * working on adding listings
    
    * Get and Post for listings; gitignore change for vscode
    
    * listing status and endpoints for changing it to hidden or archived
    
    * Unavailability Periods
    
    * all endpoints for listings logic except photos, no validation
    
    * Logic moved from listings repository to service
    
    * validation of listing properties
    
    * ListingsController now checks for role when authorizing
    
    * Endpoint for getting all ids owned by user
    
    * Querrying listings endpoint
    
    * Listings' photos operations
    
    * Updated gitignore
    
    * Fixed authorization and added ID field to ListingDTO

---
+++ src/backend/FlatShareBackend/Migrations/20260410122755_photos.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FlatShareBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class photos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<List<Guid>>(
+                name: "Photos",
+                table: "Listings",
+                type: "uuid[]",
+                nullable: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Photos",
+                table: "Listings");
+        }
+    }
+}
