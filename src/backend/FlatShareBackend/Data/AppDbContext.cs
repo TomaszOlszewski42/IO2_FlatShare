9355e6828bf6970a1073db6d61d8f7df523d44e6
Tomasz Olszewski <tomaszolszewski42@gmail.com>
2026-04-12 14:22:22 +0200

    Feature/creating listings (#20)
    * Created model for listings, configured db context and create few repositories and services
    
    * working on adding listings
    
    * Get and Post for listings; gitignore change for vscode
    
    * listing status and endpoints for changing it to hidden or archived
    
    * Unavailability Periods
    
    * all endpoints for listings logic except photos, no validation
    
    * Logic moved from listings repository to service
    
    * validation of listing properties
    
    * ListingsController now checks for role when authorizing
    
    * Endpoint for getting all ids owned by user
    
    * Querrying listings endpoint
    
    * Listings' photos operations
    
    * Updated gitignore
    
    * Fixed authorization and added ID field to ListingDTO

--- src/backend/FlatShareBackend/Data/AppDbContext.cs
+++ src/backend/FlatShareBackend/Data/AppDbContext.cs
@@ -11,10 +11,26 @@ namespace FlatShareBackend.Data
 
         public DbSet<User> Users => Set<User>();
         public DbSet<UserSession> Sessions => Set<UserSession>();
+        public DbSet<Listing> Listings => Set<Listing>();
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Listing>(entity =>
+            {
+                entity.HasIndex(x => x.Id).IsUnique();
+
+                entity.HasOne(x => x.Owner)
+                    .WithMany()
+                    .HasForeignKey(x => x.OwnerId)
+                    .IsRequired()
+                    .OnDelete(DeleteBehavior.Cascade);
+
+                entity.OwnsOne(x => x.Location);
+
+                entity.OwnsMany(x => x.UnavailableDates);
+            });
 
             modelBuilder.Entity<User>(entity =>
             {
---
+++ src/backend/FlatShareBackend/Exceptions/DataBaseExceptions.cs
@@ -0,0 +1,7 @@
+﻿namespace FlatShareBackend.Exceptions;
+
+public class InvalidIdException(string msg) : Exception(msg);
+
+public class UnauthorizedDatabaseOperation(string msg) : Exception(msg);
+
+public class ListingValidationException(string msg) : Exception(msg);
