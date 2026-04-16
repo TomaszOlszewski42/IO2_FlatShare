5a5538efbd57fb8373a919b6e2d6b2a203756227
Tomasz Olszewski <tomaszolszewski42@gmail.com>
2026-04-16 19:14:34 +0200

    Listing Attributes

---
+++ src/backend/FlatShareBackend/Migrations/20260416171007_ListingAttributes.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FlatShareBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class ListingAttributes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Attributes_CloseToShops",
+                table: "Listings",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Attributes_NonSmokingOnly",
+                table: "Listings",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Attributes_PetsAllowed",
+                table: "Listings",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Attributes_Profile",
+                table: "Listings",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Attributes_CloseToShops",
+                table: "Listings");
+
+            migrationBuilder.DropColumn(
+                name: "Attributes_NonSmokingOnly",
+                table: "Listings");
+
+            migrationBuilder.DropColumn(
+                name: "Attributes_PetsAllowed",
+                table: "Listings");
+
+            migrationBuilder.DropColumn(
+                name: "Attributes_Profile",
+                table: "Listings");
+        }
+    }
+}
