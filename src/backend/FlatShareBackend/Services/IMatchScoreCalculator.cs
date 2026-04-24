ee6e841bad026ffa2639495317d516c12da48ade
Tomasz Olszewski <tomaszolszewski42@gmail.com>
2026-04-24 15:08:59 +0200

    FLA 34: Paging refactored to be done in database

--- src/backend/FlatShareBackend/Services/IMatchScoreCalculator.cs
+++ src/backend/FlatShareBackend/Services/IMatchScoreCalculator.cs
@@ -1,8 +1,14 @@
+using System.Linq.Expressions;
 using FlatShareBackend.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace FlatShareBackend.Services;
 
 public interface IMatchScoreCalculator
 {
-    public float Calculate(Listing listing, UserPreferences preferences);
+    // returns lambda expression, for example
+    // return x => 
+    //         ((x.Price <= pref.MaxPrice ? 1.0 : 0.0) * pref.PriceWeight) +
+    //         ((x.City == pref.City ? 1.0 : 0.0) * pref.LocationWeight);
+    public Expression<Func<Listing, float>> GetScoreExpression(UserPreferences preferences);
 }
