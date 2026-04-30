71b0cd96cc4289d16f2a93bbe08c888ac345e8e6
Tomasz Olszewski <tomaszolszewski42@gmail.com>
2026-04-30 17:45:34 +0200

    Float profile attribute is enum; added needed json converters

--- src/backend/FlatShareBackend/AgregationClasses/PagingQueryArgs.cs
+++ src/backend/FlatShareBackend/AgregationClasses/PagingQueryArgs.cs
@@ -24,7 +24,7 @@ public class PagingQueryArgs
     public bool? NonSmokingOnly { get; set; }
     public bool? PetsAllowed { get; set; }
     public bool? CloseToShops { get; set; }
-    public string? Profile { get; set; }
+    public UserProfile? Profile { get; set; }
     public decimal? MinArea { get; set; }
     public decimal? MaxArea { get; set; }
     public DateOnly? StartDate { get; set; }
---
+++ src/backend/FlatShareBackend/Migrations/20260428133347_ProfilesEnum.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FlatShareBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProfilesEnum : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Attributes_Profile",
+                table: "Listings",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Attributes_Profile",
+                table: "Listings",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+    }
+}
