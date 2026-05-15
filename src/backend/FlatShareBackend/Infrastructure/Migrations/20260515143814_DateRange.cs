4e4dd6de01145087b6ef301118606f0e23ef476a
Tomasz Olszewski <tomaszolszewski42@gmail.com>
2026-05-15 16:41:30 +0200

    MissingMigration for DateRange

---
+++ src/backend/FlatShareBackend/Infrastructure/Migrations/20260515143814_DateRange.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FlatShareBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class DateRange : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "To",
+                table: "DateRange",
+                newName: "Until");
+
+            migrationBuilder.RenameColumn(
+                name: "From",
+                table: "DateRange",
+                newName: "Since");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Message",
+                table: "DateRange",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Message",
+                table: "DateRange");
+
+            migrationBuilder.RenameColumn(
+                name: "Until",
+                table: "DateRange",
+                newName: "To");
+
+            migrationBuilder.RenameColumn(
+                name: "Since",
+                table: "DateRange",
+                newName: "From");
+        }
+    }
+}
